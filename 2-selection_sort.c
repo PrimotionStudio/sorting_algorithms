4612a2134a1c8c0d8ca810198838bc39384b89bd
The Primotion Studio <oyedelenewton@gmail.com>
2024-01-22 01:28:01 +0100

    0

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Usign selection sort method
+ * to sort an array in c, COOL!!!
+ * @array: The array to be sorted
+ * @size: The lenght of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int mn, mni, tmp, i, j;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		for (j = i; j < (int)size; j++)
+		{
+			if (j == i || array[j] < mn)
+			{
+				mn = array[j];
+				mni = j;
+				if (j == i)
+					continue;
+			}
+		}
+		tmp = array[i];
+		array[i] = mn;
+		array[mni] = tmp;
+		print_array(array, size);
+	}
+}
